4a7eb32323cd83249722ca548cd0e59bbdba017f
Jemima-Harisoa <jemimarazafimandimby@gmail.com>
2025-11-11 08:04:34 +0300

    Sprint 4 Model.View + Execution fonction mapper dans Redirection servlet

--- src/main/java/annotations/View.java
+++ src/main/java/annotations/View.java
@@ -5,8 +5,8 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.FIELD)
-public @interface AnnotationAttribut {
-
-    
+@Target(ElementType.TYPE)
+public @interface View {
+    String name() default "";
+    String template() default "";
 }
---
+++ src/main/java/model/View.java
@@ -0,0 +1,43 @@
+package model;
+
+public class View {
+    private String name;
+    private String template;
+    private Object data;    
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+    public Object getData() {
+        return data;
+    }
+
+    public void setTemplate(String template){
+        this.template = template;
+    }
+    public String getTemplate() {
+        return template;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public View(String name, String template) {
+        setName(name);
+        setTemplate(template);
+    }
+    
+    public View(String name, String template, Object data) {
+        setName(name);
+        setTemplate(template);
+        setData(data);
+    }
+
+    public View() {
+    }
+
+}
